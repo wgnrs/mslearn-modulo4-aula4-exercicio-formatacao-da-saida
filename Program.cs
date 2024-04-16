d8af5206b270312dd4a40b80fe08ba24b6aea95e
Wagner <wagner_silvajr@hotmail.com>
2024-04-16 09:03:52 -0300

    formatacao de percentuais

--- Program.cs
+++ Program.cs
@@ -5,16 +5,22 @@
 // Console.WriteLine("{0} {0} {0}!", first, second);
 
 // // Interpolação
-
 // Console.WriteLine($"{first} {second}!");
 // Console.WriteLine($"{second} {first}!");
 // Console.WriteLine($"{first} {first} {first}!");
 
+// Especificadores de formato ------------------------------------------------------------------------
+
+// // Formatacao em real R$
 // decimal price = 123.45m;
 // int discount = 50;
 // Console.WriteLine($"Price: {price:C} (Save {discount:C})");
 
-decimal measurement = 123456.78912m;
-Console.WriteLine($"Measurement: {measurement:N} units");
+// // Formatacao de casas depois da virgula ","
+// decimal measurement = 123456.78912m;
+// Console.WriteLine($"Measurement: {measurement:N} units");
+// Console.WriteLine($"Measurement: {measurement:N4} units");
 
-Console.WriteLine($"Measurement: {measurement:N4} units");
+// // Formatacao de percentuais
+decimal tax = .36785m;
+Console.WriteLine($"Tax rate: {tax:P2}");
--- obj/Debug/net8.0/mslearn-modulo4-aula4-exercicio-formatacao-da-saida.AssemblyInfo.cs
+++ obj/Debug/net8.0/mslearn-modulo4-aula4-exercicio-formatacao-da-saida.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mslearn-modulo4-aula4-exercicio-formatacao-da-saida")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ebbb61f4cf16cb244ec462cd083749a40cb2403")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89523a12edd2b94b53b408bf2a113d7440b3dbd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("mslearn-modulo4-aula4-exercicio-formatacao-da-saida")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mslearn-modulo4-aula4-exercicio-formatacao-da-saida")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
