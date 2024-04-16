27bc9279342832cdcd3c8cfc157cead645b710e4
Wagner <wagner_silvajr@hotmail.com>
2024-04-16 08:56:17 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string first = "Hello";
+string second = "World";
+Console.WriteLine("{0} {1}!", first, second);
+Console.WriteLine("{1} {0}!", first, second);
+Console.WriteLine("{0} {0} {0}!", first, second);
+
+// Interpolação
+
+Console.WriteLine($"{first} {second}!");
+Console.WriteLine($"{second} {first}!");
+Console.WriteLine($"{first} {first} {first}!");
