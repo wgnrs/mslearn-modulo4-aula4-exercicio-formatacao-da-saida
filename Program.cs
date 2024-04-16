3ebbb61f4cf16cb244ec462cd083749a40cb2403
Wagner <wagner_silvajr@hotmail.com>
2024-04-16 08:58:25 -0300

    formatacao em real R$

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
-﻿string first = "Hello";
-string second = "World";
-Console.WriteLine("{0} {1}!", first, second);
-Console.WriteLine("{1} {0}!", first, second);
-Console.WriteLine("{0} {0} {0}!", first, second);
+﻿// string first = "Hello";
+// string second = "World";
+// Console.WriteLine("{0} {1}!", first, second);
+// Console.WriteLine("{1} {0}!", first, second);
+// Console.WriteLine("{0} {0} {0}!", first, second);
 
-// Interpolação
+// // Interpolação
 
-Console.WriteLine($"{first} {second}!");
-Console.WriteLine($"{second} {first}!");
-Console.WriteLine($"{first} {first} {first}!");
+// Console.WriteLine($"{first} {second}!");
+// Console.WriteLine($"{second} {first}!");
+// Console.WriteLine($"{first} {first} {first}!");
+
+decimal price = 123.45m;
+int discount = 50;
+Console.WriteLine($"Price: {price:C} (Save {discount:C})");
